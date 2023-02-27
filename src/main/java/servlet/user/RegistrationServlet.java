4a0d87dbaaae77d2cbfd504bb62ad42c19ec0843
Arina <arina.milyutina.0005@gmail.com>
2023-02-27 22:36:29 +0300

    add validator

--- src/main/java/servlet/user/RegistrationServlet.java
+++ src/main/java/servlet/user/RegistrationServlet.java
@@ -3,6 +3,7 @@ package servlet.user;
 
 import entity.User;
 import service.UserService;
+import validator.UserValidator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 
 @WebServlet(name = "Reg", value = "/reg")
@@ -23,15 +25,15 @@ public class RegistrationServlet extends HttpServlet {
         String name = req.getParameter("name");
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        User user = new User();
-        user.setName(name);
-        user.setUsername(username);
-        user.setPassword(password);
-        userService.create(user);
-        if (name == null | username == null | password == null) {
-            resp.getWriter().println("Registration failed");
-        } else {
+        if (name != null | username != null | password != null && UserValidator.isValidUsername(Objects.requireNonNull(username)) && UserValidator.isValidPassword(Objects.requireNonNull(password))) {
+            User user = new User();
+            user.setName(name);
+            user.setUsername(username);
+            user.setPassword(password);
+            userService.create(user);
             resp.getWriter().println("Registration is completed.");
+        } else {
+            resp.getWriter().println("Registration failed. Check the correctness of the entered data!");
         }
     }
 }
