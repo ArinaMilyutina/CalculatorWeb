65758dc244d2190c5c24fa4793c572d3823918cb
Arina <arina.milyutina.0005@gmail.com>
2023-03-01 15:08:57 +0300

    correct JDBC

--- src/main/java/servlet/operation/DeleteOperationServlet.java
+++ src/main/java/servlet/operation/DeleteOperationServlet.java
@@ -1,6 +1,5 @@
 package servlet.operation;
 
-import entity.Operation;
 import service.CalculatorService;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/deleteOperation")
 public class DeleteOperationServlet extends HttpServlet {
