95d04360c8d029fea2c21c9c027e84a8f69a5a98
Arina <arina.milyutina.0005@gmail.com>
2023-03-06 01:28:55 +0300

    add Calculator

---
+++ src/main/java/service/Calculator.java
@@ -0,0 +1,22 @@
+package service;
+
+public class Calculator {
+    double sum(double a, double b) {
+        return a + b;
+    }
+
+
+    double res(double a, double b) {
+        return a - b;
+    }
+
+
+    double div(double a, double b) {
+        return a / b;
+    }
+
+
+    double mul(double a, double b) {
+        return a * b;
+    }
+}
