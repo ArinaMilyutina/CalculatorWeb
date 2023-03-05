77ef64a246f86e63f16dbf73c823c237a13d0900
Arina <arina.milyutina.0005@gmail.com>
2023-03-06 01:39:40 +0300

    change regex

--- src/main/java/validator/UserValidator.java
+++ src/main/java/validator/UserValidator.java
@@ -1,8 +1,8 @@
 package validator;
 
 public class UserValidator {
-    private static final String USERNAME = "[a-zA-Z]{4,8}$";
-    private static final String PASSWORD = "[0-9]{8}$";
+    private static final String USERNAME = "^[a-zA-Z][a-zA-Z0-9-_.]{1,10}$";
+    private static final String PASSWORD = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$";
 
     public static boolean isValidUsername(String username) {
         return username.matches(USERNAME);
