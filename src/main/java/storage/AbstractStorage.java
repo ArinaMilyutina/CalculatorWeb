9ec1cee789a6bfcf05424899e166d95c50af5b87
Arina <arina.milyutina.0005@gmail.com>
2023-03-22 16:51:12 +0300

    add AbstractStorage

---
+++ src/main/java/storage/AbstractStorage.java
@@ -0,0 +1,29 @@
+package storage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class AbstractStorage {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "arinemiller22";
+    private final Connection connection;
+
+    public AbstractStorage() {
+        this.connection = createConnection();
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    private Connection createConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
